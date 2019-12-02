49c42233ab5a6d7d23c6171b9335c55533d5c2e7
callielinux <callielinux@gmail.com>
2019-12-01 20:39:24 -0500

    refactor: Add puzzle utilities

--- app.js
+++ app.js
@@ -1,117 +1,31 @@
 // Import required packages
-var express = require("express");
-var exphbs = require("express-handlebars");
-var fs = require("fs");
-var loki = require("lokijs");
-
-// Create Puzzle DB and insert puzzles
-var db = new loki("Puzzle");
-var puzzles = db.addCollection("puzzles", { indices: ["title"] });
-var puzzleData = JSON.parse(fs.readFileSync("./src/models/puzzle.json", "utf-8"));
-puzzleData.forEach(function(puzzle) {
-  puzzles.insert(puzzle);
-});
-
-// Grab all available days in puzzles
-var days15 = [];
-puzzles
-  .chain()
-  .find({ year: "2015" })
-  .compoundsort(["day", true])
-  .data()
-  .forEach(function(puzzle15) {
-    days15.push(puzzle15.day);
-  });
-
-var days16 = [];
-puzzles
-  .chain()
-  .find({ year: "2016" })
-  .compoundsort(["day", true])
-  .data()
-  .forEach(function(puzzle16) {
-    days16.push(puzzle16.day);
-  });
-
-
-var days17 = [];
-puzzles
-  .chain()
-  .find({ year: "2017" })
-  .compoundsort(["day", true])
-  .data()
-  .forEach(function(puzzle17) {
-    days17.push(puzzle17.day);
-  });
-
-  var days18 = [];
-  puzzles
-    .chain()
-    .find({ year: "2018" })
-    .compoundsort(["day", true])
-    .data()
-    .forEach(function(puzzle18) {
-      days18.push(puzzle18.day);
-    });
-
-var port = process.env.PORT || 3000;
+var express = require('express');
+var exphbs = require('express-handlebars');
+var puzzle = require('./src/utils/puzzle');
 
 // Configure express server
 var app = express();
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('views', './src/views/');
-app.set("view engine", "handlebars");
+app.set('view engine', 'handlebars');
 
-app.use(express.static("src/static"));
-app.use(express.static("./node_modules/bootstrap/dist/"));
+app.use(express.static('src/static'));
+app.use(express.static('./node_modules/bootstrap/dist/'));
 
 // Configure router
 var router = express.Router();
 
-router.route("/").get(function(req, res) {
-  var puzzle = puzzles.findOne({ $and: [{ year: "2015" }, { day: "1" }] });
-  var solutions = require("./src/modules/" +
-    puzzle.year +
-    "/day" +
-    puzzle.day +
-    "_solution");
-
-  res.render("day", {
-    p1Solution: solutions.p1Solution(puzzle.input),
-    p2Solution: solutions.p2Solution(puzzle.input),
-    days2015: days15,
-    days2016: days16,
-    days2017: days17,
-    days2018: days18,
-    dayTitle: puzzle.title,
-    dayNumber: puzzle.day
-  });
+router.route('/').get(function(_, res) {
+  res.render('day', puzzle.getPuzzleByDay('2015', '1'));
 });
 
-router.route(["/:year/day/:day"]).get(function(req, res) {
-  var puzzle = puzzles.findOne({
-    $and: [{ year: req.params.year }, { day: req.params.day }]
-  });
-  var solutions = require("./src/modules/" +
-    puzzle.year +
-    "/day" +
-    puzzle.day +
-    "_solution");
-
-  res.render("day", {
-    p1Solution: solutions.p1Solution(puzzle.input),
-    p2Solution: solutions.p2Solution(puzzle.input),
-    days2015: days15,
-    days2016: days16,
-    days2017: days17,
-    days2018: days18,
-    dayTitle: puzzle.title,
-    dayNumber: puzzle.day
-  });
+router.route(['/:year/day/:day']).get(function(req, res) {
+  res.render('day', puzzle.getPuzzleByDay(req.params.year, req.params.day));
 });
 
-app.use("/", router);
+app.use('/', router);
 
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log("Server listening on port " + port);
+  console.log('Server listening on port ' + port);
 });
---
+++ src/utils/puzzle.js
@@ -0,0 +1,50 @@
+var fs = require('fs');
+var loki = require('lokijs');
+const db = new loki('Puzzle', {
+  autoload: true,
+  autoloadCallback: initialize
+});
+
+function initialize() {
+  // Creates Puzzle DB and insert puzzles
+  let puzzles = db.getCollection('puzzles');
+  if (puzzles === null) {
+    puzzles = db.addCollection('puzzles', { indices: ['title'] });
+  }
+  JSON.parse(fs.readFileSync('./src/models/puzzle.json', 'utf-8')).forEach(function(puzzle) {
+    puzzles.insert(puzzle);
+  });
+}
+
+function loadPuzzlesByYear(puzzles, year) {
+  // Loads each available solution for a particular year
+  return puzzles
+    .chain()
+    .find({ year: year })
+    .compoundsort(['day', true])
+    .data()
+    .map(puzzle => puzzle.day);
+}
+
+function getPuzzleByDay(year, day) {
+  // Creates a handlebars config for a particular day
+  let puzzles = db.getCollection('puzzles');
+
+  const puzzle = puzzles.findOne({ $and: [{ year }, { day }] });
+  const solutions = require('../modules/' + puzzle.year + '/day' + puzzle.day + '_solution');
+
+  return {
+    p1Solution: solutions.p1Solution(puzzle.input),
+    p2Solution: solutions.p2Solution(puzzle.input),
+    dayTitle: puzzle.title,
+    dayNumber: puzzle.day,
+    days2015: loadPuzzlesByYear(puzzles, '2015'),
+    days2016: loadPuzzlesByYear(puzzles, '2016'),
+    days2017: loadPuzzlesByYear(puzzles, '2017'),
+    days2018: loadPuzzlesByYear(puzzles, '2018')
+  };
+}
+
+module.exports = {
+  getPuzzleByDay
+};
