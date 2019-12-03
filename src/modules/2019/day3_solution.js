2dc3f784f00be46e0c47d55973cbf96f483047a1
Callie Toothman <callie.toothman@ncino.com>
2019-12-03 15:55:05 -0500

    feat: Add 2019 Day 3 Part 1 Solution

--- src/modules/2019/day3_solution.js
+++ src/modules/2019/day3_solution.js
@@ -1,54 +1,53 @@
-const getIntersections = function(path1, path2) {
-  const getCoordinates = (path) => path.reduce((coordinates, instruction) => {
-    coordinates.push(...getSubCoordinates(coordinates[coordinates.length - 1], instruction));
-    return coordinates;
-  }, ['0,0']);
+const getCoordinates = function(path) {
+  return path.reduce(
+    (coordinates, instruction) => {
+      coordinates.push(...getSubCoordinates(coordinates[coordinates.length - 1], instruction));
+      return coordinates;
+    },
+    ['0,0']
+  );
+};
 
+const getIntersections = function(path1, path2) {
   const path1Coordinates = new Set(getCoordinates(path1).slice(1));
   const path2Coordinates = new Set(getCoordinates(path2).slice(1));
 
-  // Intersections: (6, 5) and (3, 3)
   const intersections = new Set([...path1Coordinates].filter(x => path2Coordinates.has(x)));
   return Array.from(intersections).map(x => x.split(',').map(y => parseInt(y)));
 };
 
 const getSubCoordinates = function(coordinate, instruction) {
-  const getInstructionValue = (i) => parseInt(i.replace(/[UDLR]/u, ''));
-  let subCoordinates = [];
+  const getInstructionValue = i => parseInt(i.replace(/[UDLR]/u, ''));
 
-  if(instruction.startsWith('U')) {
-    for(let i = 1; i <= getInstructionValue(instruction); i++) {
+  const calculateSubCoordinates = (coordinate, coordinateIdx, instruction, shouldNegate) => {
+    let subCoordinates = [];
+    for (let i = 1; i <= getInstructionValue(instruction); i++) {
       const nextCoordinate = coordinate.split(',').map(x => parseInt(x));
-      nextCoordinate[1] += i;
+      nextCoordinate[coordinateIdx] += shouldNegate ? i * -1 : i;
       subCoordinates.push(nextCoordinate.join(','));
     }
+    return subCoordinates;
+  };
+
+  if (instruction.startsWith('U')) {
+    return calculateSubCoordinates(coordinate, 1, instruction, false);
   } else if (instruction.startsWith('D')) {
-    for(let i = 1; i <= getInstructionValue(instruction); i++) {
-      const nextCoordinate = coordinate.split(',').map(x => parseInt(x));
-      nextCoordinate[1] -= i;
-      subCoordinates.push(nextCoordinate.join(','));
-    }
+    return calculateSubCoordinates(coordinate, 1, instruction, true);
   } else if (instruction.startsWith('L')) {
-    for(let i = 1; i <= getInstructionValue(instruction); i++) {
-      const nextCoordinate = coordinate.split(',').map(x => parseInt(x));
-      nextCoordinate[0] -= i;
-      subCoordinates.push(nextCoordinate.join(','));
-    }
+    return calculateSubCoordinates(coordinate, 0, instruction, true);
   } else {
-    for(let i = 1; i <= getInstructionValue(instruction); i++) {
-      const nextCoordinate = coordinate.split(',').map(x => parseInt(x));
-      nextCoordinate[0] += i;
-      subCoordinates.push(nextCoordinate.join(','));
-    }
+    return calculateSubCoordinates(coordinate, 0, instruction, false);
   }
-
-  return subCoordinates;
 };
 
 const getDistanceToClosestIntersection = function(input) {
   const [path1, path2] = input.split('\n');
   const intersections = getIntersections(path1.split(','), path2.split(','));
-  return 6;
+
+  return intersections.reduce((shortestDistance, location) => {
+    const currentDistance = location.reduce((acc, val) => Math.abs(acc) + Math.abs(val));
+    return currentDistance < shortestDistance ? currentDistance : shortestDistance;
+  }, Number.MAX_SAFE_INTEGER);
 };
 
 const tbd = function(input) {
