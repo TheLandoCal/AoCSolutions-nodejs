d3c8efbd4008048e205d0de1e55f59c52da2be9a
callielinux <callielinux@gmail.com>
2019-12-07 23:30:45 -0500

    feat: Add preliminary 2019 Day 7 Part 2 solution

--- src/modules/2019/day7_solution.js
+++ src/modules/2019/day7_solution.js
@@ -1,58 +1,98 @@
 const permutations = require('../2015/day9_solution').permutations;
 
-const runOpcode = function(rawInstructions, inputSignal = null, phaseSetting = null) {
+const instructionValuesMap = { 1: 4, 2: 4, 3: 2, 4: 2, 5: 3, 6: 3, 7: 4, 8: 4, 99: 1 };
+const ampNames = ['A', 'B', 'C', 'D', 'E'];
+
+const initializeAmpOutputs = function() {
+  return ampNames.reduce((outputs, ampName) => {
+    outputs[ampName] = [];
+    return outputs;
+  }, {});
+};
+
+let ampOutputs = initializeAmpOutputs();
+
+const getInputSignal = function(ampName, idx) {
+  const maxRetries = 200;
+  let numRetries = 0;
+  return new Promise(async (resolve, reject) => {
+    const numAmps = ampNames.length;
+    const incomingAmpName = ampNames[(numAmps + ampNames.indexOf(ampName) - 1) % numAmps];
+
+    while (ampOutputs[incomingAmpName][idx] == null) {
+      if (numRetries >= maxRetries) {
+        reject(
+          `Max Retries reached (${maxRetries}) waiting for output from Amp ${incomingAmpName} into Amp ${ampName}`
+        );
+      }
+      numRetries++;
+      await new Promise(resolve => setTimeout(resolve, 250));
+    }
+
+    resolve(ampOutputs[incomingAmpName][idx]);
+  });
+};
+
+const getInstructionParameters = function(instructions, pointer, numParameters) {
+  const opcode = getOpcode(instructions, pointer);
+  return instructions.slice(pointer + 1, pointer + numParameters).map((p, idx) => {
+    return { mode: opcode[2 - idx], value: p };
+  });
+};
+
+const getOpcode = function(instructions, pointer) {
+  return instructions[pointer]
+    .toString()
+    .padStart(5, '0')
+    .split('')
+    .map(x => parseInt(x));
+};
+
+const runOpcode = async function(rawInstructions, ampName, phaseSetting, loopMode = false) {
   const instructions = rawInstructions.split(',').map(code => parseInt(code));
-  const instructionValuesMap = {
-    1: 4,
-    2: 4,
-    3: 2,
-    4: 2,
-    5: 3,
-    6: 3,
-    7: 4,
-    8: 4,
-    99: 1
-  };
-
   let instructionPointer = 0;
-  let phaseSettingUsed = phaseSetting == null;
-  let outputs = [];
+  let numInputRequests = 0;
   while (instructions[instructionPointer] !== 99) {
-    const opcode = instructions[instructionPointer]
-      .toString()
-      .padStart(5, '0')
-      .split('')
-      .map(x => parseInt(x));
-
+    const opcode = getOpcode(instructions, instructionPointer);
     const operation = parseInt(opcode.slice(3).join(''));
     let numInstructionValues = instructionValuesMap[operation];
-    const input = !phaseSettingUsed ? phaseSetting : inputSignal;
-    const parameters = instructions
-      .slice(instructionPointer + 1, instructionPointer + numInstructionValues)
-      .map((p, idx) => {
-        return { mode: opcode[2 - idx], value: p };
-      });
+    const parameters = getInstructionParameters(
+      instructions,
+      instructionPointer,
+      numInstructionValues
+    );
 
-    const result = runInstruction(input, operation, parameters, instructions);
+    let input;
+    if (operation === 3) {
+      if (numInputRequests == 0) {
+        input = phaseSetting;
+      } else if (numInputRequests == 1 && ampName === 'A') {
+        input = 0;
+      } else {
+        const outputIdx = loopMode ? numInputRequests - (ampName === 'A' ? 2 : 1) : 0;
+        input = await getInputSignal(ampName, outputIdx);
+      }
+      numInputRequests++;
+    }
+
+    const result = runInstruction(operation, parameters, instructions, input);
     if (result != null) {
       if ([4].includes(operation)) {
         // Handle output-based opcodes
-        outputs.push(result);
+        ampOutputs[ampName].push(result);
       } else if ([5, 6].includes(operation)) {
         // Handle Jump-based opcodes
         instructionPointer = result;
         numInstructionValues = 0;
       }
     }
-
-    phaseSettingUsed = phaseSettingUsed || operation === 3;
     instructionPointer += numInstructionValues;
   }
 
-  return outputs;
+  return ampOutputs[ampName];
 };
 
-const runInstruction = function(input, operation, parameters, instructions) {
+const runInstruction = function(operation, parameters, instructions, input) {
   // Since parameter modes are stored in the same value as the instruction's opcode,
   // single digit opcodes mean that all parameters are in position mode.
   const getParameterValue = parameter => {
@@ -81,20 +121,51 @@ const runInstruction = function(input, operation, parameters, instructions) {
   return null;
 };
 
-const findMaxThrusterSignal = function(input) {
-  const phaseSettingSequences = permutations([0, 1, 2, 3, 4]);
+const findMaxThrusterSignal = async function(input, phaseValues, loopMode) {
+  const phaseSettingSequences = permutations(phaseValues);
+  // const phaseSettingSequences = [
+  //   [0, 1, 2, 3, 4],
+  //   [2, 0, 1, 4, 3]
+  // ];
 
-  return phaseSettingSequences.reduce((maxThrusterSignal, phaseSettingSequence) => {
-    const thrusterSignal = phaseSettingSequence.reduce((inputSignal, amplifierPhaseSetting) => {
-      const outputs = runOpcode(input, inputSignal, amplifierPhaseSetting);
-      return outputs[outputs.length - 1];
-    }, 0);
+  let maxThrusterSignal = Number.MIN_SAFE_INTEGER;
+  await phaseSettingSequences.reduce(async (previousPromise, phaseSettingSequence) => {
+    await previousPromise;
+    ampOutputs = initializeAmpOutputs();
+    await Promise.all([
+      runOpcode(input, ampNames[0], phaseSettingSequence[0], loopMode),
+      runOpcode(input, ampNames[1], phaseSettingSequence[1], loopMode),
+      runOpcode(input, ampNames[2], phaseSettingSequence[2], loopMode),
+      runOpcode(input, ampNames[3], phaseSettingSequence[3], loopMode),
+      runOpcode(input, ampNames[4], phaseSettingSequence[4], loopMode)
+    ])
+      .then(outputs => {
+        const outputE = outputs.pop().pop();
+        maxThrusterSignal = Math.max(maxThrusterSignal, outputE);
+      })
+      .catch(reason => {
+        console.log(`ERROR: ${reason}`);
+      });
+  }, Promise.resolve());
 
-    return thrusterSignal > maxThrusterSignal ? thrusterSignal : maxThrusterSignal;
-  }, Number.MIN_SAFE_INTEGER);
+  console.log(`Max Thruster Signal => ${maxThrusterSignal}`);
+  return maxThrusterSignal;
+};
+
+const findMaxThrusterSignalLowPhases = function(input) {
+  const phaseValues = [0, 1, 2, 3, 4];
+  findMaxThrusterSignal(input, phaseValues, false);
+  return '[...see console log for answer]';
+};
+
+const findMaxThrusterSignalHighPhases = function(input) {
+  const phaseValues = [5, 6, 7, 8, 9];
+  findMaxThrusterSignal(input, phaseValues, true);
+  return '[...see console log for answer]';
 };
 
 module.exports = {
-  p1Solution: findMaxThrusterSignal,
+  findMaxThrusterSignal,
+  p1Solution: findMaxThrusterSignalLowPhases,
   p2Solution: () => '???'
 };
--- src/modules/2019/day7_solution.test.js
+++ src/modules/2019/day7_solution.test.js
@@ -1,27 +1,71 @@
-var solutions = require('./day7_solution'),
-  findMaxThrusterSignal = solutions.p1Solution,
-  tbd2 = solutions.p2Solution;
+var findMaxThrusterSignal = require('./day7_solution').findMaxThrusterSignal;
 
 describe('#findMaxThrusterSignal', function() {
-  it('Tests that 3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0 has a max thruster signal of 43210', function() {
-    findMaxThrusterSignal('3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0').should.equal(43210);
-  });
+  this.timeout(120000);
+  // it('Tests that 3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0 has a max thruster signal of 43210', function(done) {
+  //   findMaxThrusterSignal('3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0', [0, 1, 2, 3, 4])
+  //     .then(result => {
+  //       result.should.equal(43210);
+  //       done();
+  //     })
+  //     .catch(reason => {
+  //       console.log(`ERROR: ${reason}`);
+  //     });
+  // });
 
-  it('Tests that 3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0 has a max thruster signal of 54321', function() {
+  // it('Tests that 3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0 has a max thruster signal of 54321', function(done) {
+  //   findMaxThrusterSignal(
+  //     '3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0',
+  //     [0, 1, 2, 3, 4]
+  //   )
+  //     .then(result => {
+  //       result.should.equal(54321);
+  //       done();
+  //     })
+  //     .catch(reason => {
+  //       console.log(`ERROR: ${reason}`);
+  //     });
+  // });
+
+  // it('Tests that 3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0 has a max thruster signal of 65210', function(done) {
+  //   findMaxThrusterSignal(
+  //     '3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0',
+  //     [0, 1, 2, 3, 4]
+  //   )
+  //     .then(result => {
+  //       result.should.equal(65210);
+  //       done();
+  //     })
+  //     .catch(reason => {
+  //       console.log(`ERROR: ${reason}`);
+  //     });
+  // });
+
+  it('Tests that 3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5 has a max thruster signal of 139629729', function(done) {
     findMaxThrusterSignal(
-      '3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0'
-    ).should.equal(54321);
+      '3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5',
+      [5, 6, 7, 8, 9]
+    )
+      .then(result => {
+        result.should.equal(139629729);
+        done();
+      })
+      .catch(reason => {
+        console.log(`ERROR: ${reason}`);
+      });
   });
 
-  it('Tests that 3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0 has a max thruster signal of 65210', function() {
-    findMaxThrusterSignal(
-      '3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0'
-    ).should.equal(65210);
-  });
-});
-
-describe('#tbd2', function() {
-  it('Tests that some input is some value', function() {
-    tbd2('some input').should.equal('some value');
-  });
+  // it('Tests that 3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10 has a max thruster signal of 18216', function(done) {
+  //   findMaxThrusterSignal(
+  //     '3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10',
+  //     [5, 6, 7, 8, 9]
+  //   )
+  //     .then(result => {
+  //       result.should.equal(18216);
+  //       done();
+  //     })
+  //     .catch(reason => {
+  //       console.log(`ERROR: ${reason}`);
+  //     });
+  // });
 });
