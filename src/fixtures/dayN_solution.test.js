72142178df78f4a98bc2a80dafd790733bcb26a3
callielinux <callielinux@gmail.com>
2019-12-10 23:47:21 -0500

    test: Update test template describe text

--- src/fixtures/dayN_solution.test.js
+++ src/fixtures/dayN_solution.test.js
@@ -3,14 +3,14 @@ var solutions = require('./dayN_solution'),
   tbd2 = solutions.p2Solution;
 
 describe('#tbd1', function() {
-  it('Tests that some input is some value', function() {
+  it('Tests that the result from some input is some value', function() {
     tbd1('some input').should.equal('some value');
   });
 });
 
 
 describe('#tbd2', function() {
-  it('Tests that some input is some value', function() {
+  it('Tests that the result from some input is some value', function() {
     tbd2('some input').should.equal('some value');
   });
 });
